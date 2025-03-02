6a1c6749da72db5a9f824b8fe699a8ca8081b13d
Aravind Sree Asokan <78605765+aravindsreeasokan@users.noreply.github.com>
2025-03-02 16:08:25 +0530

    Update bookreview3.php

--- bookreview3.php
+++ bookreview3.php
@@ -1,7 +1,7 @@
 <?php
 
 function generateBookReview($bookTitle,$authorname) {
-    $apiKey = 'AIzaSyCBoO8cdangwvhzIJI3l68xt5U2cVC0gnU'; // Replace with your API key
+    $apiKey = ''; // Replace with your API key
     $apiUrl = "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=" . $apiKey;
 
     $prompt = "Assume you are a  professional book reader. Your task is to write summary for book with important point. Write professional summary of the following book: " . $bookTitle."written by". $authorname;
